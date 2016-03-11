b40e88638eb3c04525e9378a869b61e32913ba27
Ramon Villar <ramon.villar@utexas.edu>
2016-03-10 19:16:18 -0600

    Lab3 event logger project

---
+++ Lab3 - EventLogger/os.d
@@ -0,0 +1,10 @@
+.\os.o: OS.c
+.\os.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\os.o: ../inc/tm4c123gh6pm.h
+.\os.o: OS.h
+.\os.o: PLL.h
+.\os.o: ST7735.h
+.\os.o: UART.h
+.\os.o: ADC.h
+.\os.o: Switch.h
+.\os.o: Lab2.h
